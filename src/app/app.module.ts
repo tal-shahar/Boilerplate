48faeb5adfdba215a5736b50c022ab6a463bd3c4
tal shahar <ths126@gmail.com>
2018-06-05 13:31:41 +0300

    adding angular matireal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatMenuModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
